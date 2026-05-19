b483468fbe6b37bac0e72f1c6933264f6490bf69
ArianaQuiterio-png <1725110151@utectulancingo.edu.mx>
2026-05-19 16:00:26 +0000

    CREATED banco.py

---
+++ ejercicios/semana2/quimera.py
@@ -0,0 +1,21 @@
+class Quimera:
+    def __init__(self, tamano, especie,color,mordedura,forma,peso,
+                 no_patas,cola,garras,melena):
+        self.tamano=tamano
+        self.especie=especie
+        self.color=color
+        self.mordedura=mordedura
+        self.forma=forma
+        self.peso=peso
+        self.no_patas=no_patas
+        self.cola=cola
+        self.garras=garras
+        self.melena=melena
+        print(f"Su tamaño es {self.tamano}")
+        print(f"Especie es {self.especie}")
+        print(f"Es de color {self.color}")
+        print(f"La mordedura es {self.mordedura}")
+        print(f"La forma es {self.forma}")
+        print(f"")
+        print(f"")
+        print(f"")
