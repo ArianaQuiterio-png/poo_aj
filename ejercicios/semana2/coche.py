33dd471151aa2b6d7e20e2443a353718f174a63d
ArianaQuiterio-png <1725110151@utectulancingo.edu.mx>
2026-05-20 00:21:12 +0000

    CREATED coche.py

---
+++ ejercicios/semana2/coche.py
@@ -0,0 +1,14 @@
+class Coche:
+    def __init__(self,marca,modelo,puertas,ventanas,llantas,
+                 capacidad,color,cajuela,aire_acondicionado,
+                 estandar):
+        self.marca=marca
+        self.modelo=modelo
+        self.puertas=puertas
+        self.ventanas=ventanas
+        self.llantas=llantas
+        self.capacidad=capacidad
+        self.color=color
+        self.cajuela=cajuela
+        self.aire_acondicionado=aire_acondicionado
+        self.estanadr=estandar
